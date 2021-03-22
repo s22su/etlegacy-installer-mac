44743b6f3f26d3d179a83952b4b79296bcc6116e
Sergei Beregov <sergei.beregov@gmail.com>
2021-03-22 19:17:56 +0200

    Update to ETLegace 2.77.1 & fix typos

--- installer.sh
+++ installer.sh
@@ -3,29 +3,29 @@
 set -eux
 
 echo "Downloading ETLegacy"
-wget https://www.etlegacy.com/download/file/233 -O etlegacy.tar.gz
+wget https://www.etlegacy.com/download/file/259 -O etlegacy.tar.gz
 
 echo "Extracting ETLegacy"
 tar -xzf etlegacy.tar.gz
-mv etlegacy-v2.77-OSX ETLegacy
+mv etlegacy-v2.77.1-macOS ETLegacy
 
 echo "Extracted to ./ETLegacy"
 
 echo "Downloading SplashDamage WolfET"
-wget https://cdn.splashdamage.com/downloads/games/wet/WolfET.2.60d.dmg -O WolfETSpalshDamage.dmg
+wget https://cdn.splashdamage.com/downloads/games/wet/WolfET.2.60d.dmg -O WolfETSplashDamage.dmg
 
 echo "Converting downloaded .dmg -> .cdr to remove license"
-hdiutil convert -quiet WolfETSpalshDamage.dmg -format UDTO -o WolfETSpalshDamageNoLicence
+hdiutil convert -quiet WolfETSplashDamage.dmg -format UDTO -o WolfETSplashDamageNoLicence
 
 echo "Mounting .cdr"
-hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint WolfETImage WolfETSpalshDamageNoLicence.cdr
+hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint WolfETImage WolfETSplashDamageNoLicence.cdr
 echo "Mounted to ./WolfETImage"
 
 echo "Copying *.pk3 files from WolfETImage -> ETLegacy"
 cp ./WolfETImage/Wolfenstein\ ET/etmain/*.pk3 ./ETLegacy/etmain/
 
 echo "Creating symlink"
-ln -s ./ETLegacy/etl.app ./StartETLegacy.app
+ln -s ./ETLegacy/ET\ Legacy.app ./StartETLegacy.app
 
 echo "DONE! To start the game run this command:"
 echo "open StartETLegacy.app"
